069cb6e20fc85b8e301fee7fd968d1e7d0ac1b23
Lukas Truemper <lukas.truemper@outlook.de>
2024-01-01 20:41:00 +0100

    MapNestModel v5 - Extensions for NVIDIA Grace

---
+++ daisytuner/profiling/metrics/nvidia_grace/l2_bandwidth.py
@@ -0,0 +1,14 @@
+# Copyright 2022-2023 ETH Zurich and the Daisytuner authors.
+import dace
+import platform
+
+from daisytuner.profiling.metrics.compund_metric import CompoundMetric
+from daisytuner.profiling.metrics.nvidia_grace.l2_volume import L2Volume
+from daisytuner.profiling.metrics.nvidia_grace.runtime import Runtime
+
+
+class L2Bandwidth(CompoundMetric):
+    def __init__(self, sdfg: dace.SDFG, hostname: str = platform.node(), cache=None):
+        super().__init__(
+            metric_a=L2Volume(sdfg, hostname), metric_b=Runtime(sdfg, hostname)
+        )
